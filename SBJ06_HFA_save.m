80ab343275c9423ac6ee3e729c5d270587c51cfc
hoycw <hoycw@berkeley.edu>
2018-12-02 19:18:45 -0800

    SBJ06 HFA ready to run

--- SBJ06_HFA_save.m
+++ SBJ06_HFA_save.m
@@ -1,18 +1,21 @@
-function SBJ08a_HFA_save(SBJ,pipeline_id,an_id)
-% Calculates high frequency activity, computes cluster-based statistics, and plots the results
-% clear all; %close all;
-% Set up paths
-addpath('/home/knight/hoycw/PRJ_Stroop/scripts/');
-addpath('/home/knight/hoycw/PRJ_Stroop/scripts/utils/');
-addpath('/home/knight/hoycw/Apps/fieldtrip/');
+function SBJ06_HFA_save(SBJ,pipeline_id,an_id)
+% Calculates high frequency activity and saves fieldtrip struct
+
+%% Set up paths
+if exist('/home/knight/','dir');root_dir='/home/knight/';ft_dir=[root_dir 'hoycw/Apps/fieldtrip/'];
+else root_dir='/Volumes/hoycw_clust/';ft_dir='/Users/colinhoy/Code/Apps/fieldtrip/';end
+
+%% Set Up Directories
+addpath([root_dir 'emodim/scripts/']);
+addpath([root_dir 'emodim/scripts/utils/']);
+addpath(ft_dir);
 ft_defaults
 
 %% Data Preparation
-SBJ_vars_cmd = ['run /home/knight/hoycw/PRJ_Stroop/scripts/SBJ_vars/' SBJ '_vars.m'];
+SBJ_vars_cmd = ['run ' root_dir 'emodim/scripts/SBJ_vars/' SBJ '_vars.m'];
 eval(SBJ_vars_cmd);
-an_vars_cmd = ['run /home/knight/hoycw/PRJ_Stroop/scripts/an_vars/' an_id '_vars.m'];
+an_vars_cmd = ['run ' root_dir 'emodim/scripts/an_vars/' an_id '_vars.m'];
 eval(an_vars_cmd);
-% eval(['run /home/knight/hoycw/PRJ_Stroop/scripts/proc_vars/' pipeline_id '_proc_vars.m']);
 
 % Load Data
 load(strcat(SBJ_vars.dirs.preproc,SBJ,'_preproc_',pipeline_id,'.mat'));
@@ -23,42 +26,13 @@ cfgs = [];
 cfgs.channel = SBJ_vars.ch_lab.ROI;
 roi = ft_selectdata(cfgs,data);
 
-%% Cut into Trials
-% Pad trial_lim_s by cfg_hfa.t_ftimwin/2 to avoid NaNs in epoch of interest
-% Add 10 ms just because trimming back down to trial_lim_s exactly leave
-% one NaN on the end, so smoothing will NaN out everything
-
-%!!! BEWARE: This is suboptimal for R-locked because trial_lim_s(1)=-0.5,
-%which adds 250ms to the time series that isn't necessary; the realign_tfr
-%function should still cut to the desired data, but it'll take longer.
-trial_lim_s_pad = [trial_lim_s(1)-max(cfg_hfa.t_ftimwin)/2 trial_lim_s(2)+max(cfg_hfa.t_ftimwin)/2+0.01];
-
-% Always normalize to pre-stimulus baseline for HFA
-bsln_events = trial_info.word_onset;
-if strcmp(event_type,'stim')
-    % Cut to desired trial_lim_s
-    roi_trl = fn_ft_cut_trials_equal_len(roi,bsln_events,trial_info.condition_n',...
-        round(trial_lim_s_pad*roi.fsample));
-elseif strcmp(event_type,'resp')
-    % Check that baseline will be included in trial_lim_s
-    if trial_lim_s(1)>bsln_lim(1)
-        error(['ERROR: trial_lim_s does not include bsln_lim for an_id = ' an_id]);
-    end
-    % Cut out to max_RT+trial_lim_s(2)+max(cfg_hfa.t_ftimwin)
-    max_RT  = max(trial_info.response_time);
-    roi_trl = fn_ft_cut_trials_equal_len(roi,bsln_events,trial_info.condition_n',...
-        round([trial_lim_s_pad(1) max_RT+trial_lim_s_pad(2)]*roi.fsample));
-else
-    error(['Unknown event_type: ' event_type]);
-end
-
 %% Compute HFA
 fprintf('===================================================\n');
 fprintf('------------------ HFA Calculations ---------------\n');
 fprintf('===================================================\n');
 if strcmp(HFA_type,'multiband')
     cfg_hfa.trials = 'all';
-    hfa = ft_freqanalysis(cfg_hfa, roi_trl);
+    hfa = ft_freqanalysis(cfg_hfa, roi);
 elseif strcmp(HFA_type,'broadband')
     error('Stop using broadband and use multitapers you dummy!');
     %         % Filter to HFA band
@@ -73,36 +47,23 @@ else
     error('Unknown HFA_type provided');
 end
 
-% Trim back down to original trial_lim_s to exclude NaNs
-if strcmp(event_type,'stim')
-    cfg_trim = [];
-    cfg_trim.latency = trial_lim_s;
-    hfa = ft_selectdata(cfg_trim,hfa);
-elseif strcmp(event_type,'resp')
-    cfg_trim = [];
-    cfg_trim.latency = [trial_lim_s(1) max_RT+trial_lim_s(2)];
-    hfa = ft_selectdata(cfg_trim,hfa);
-else
-    error(['Unknown event_type: ' event_type]);
-end
-
-%% Baseline Correction
-fprintf('===================================================\n');
-fprintf('---------------- Baseline Correction --------------\n');
-fprintf('===================================================\n');
-switch bsln_type
-    case {'zboot', 'zscore'}
-        hfa = fn_bsln_ft_tfr(hfa,bsln_lim,bsln_type,n_boots);
-    case {'relchange', 'demean', 'my_relchange'}
-        error(['bsln_type ' bsln_type ' is not compatible with one-sample t test bsln activation stats']);
-%         cfgbsln = [];
-%         cfgbsln.baseline     = bsln_lim;
-%         cfgbsln.baselinetype = bsln_type;
-%         cfgbsln.parameter    = 'powspctrm';
-%         hfa = ft_freqbaseline(cfgbsln,hfa);
-    otherwise
-        error(['No baseline implemented for bsln_type: ' bsln_type]);
-end
+% %% Baseline Correction
+% fprintf('===================================================\n');
+% fprintf('---------------- Baseline Correction --------------\n');
+% fprintf('===================================================\n');
+% switch bsln_type
+%     case {'zboot', 'zscore'}
+%         hfa = fn_bsln_ft_tfr(hfa,bsln_lim,bsln_type,n_boots);
+%     case {'relchange', 'demean', 'my_relchange'}
+%         error(['bsln_type ' bsln_type ' is not compatible with one-sample t test bsln activation stats']);
+% %         cfgbsln = [];
+% %         cfgbsln.baseline     = bsln_lim;
+% %         cfgbsln.baselinetype = bsln_type;
+% %         cfgbsln.parameter    = 'powspctrm';
+% %         hfa = ft_freqbaseline(cfgbsln,hfa);
+%     otherwise
+%         error(['No baseline implemented for bsln_type: ' bsln_type]);
+% end
 
 %% Smooth Power Time Series
 if smooth_pow_ts
@@ -134,15 +95,8 @@ if strcmp(HFA_type,'multiband')
     hfa = ft_selectdata(cfg_avg,hfa);
 end
 
-%% Re-align to event of interest if necessary (e.g., response)
-if strcmp(event_type,'resp')
-    hfa = fn_realign_tfr_s2r(hfa,trial_info.response_time,trial_lim_s);
-elseif ~strcmp(event_type,'stim')
-    error(['ERROR: unknown event_type ' event_type]);
-end
-
 %% Save Results
-data_out_filename = strcat(SBJ_vars.dirs.proc,SBJ,'_HFA_ROI_',an_id,'.mat');
+data_out_filename = strcat(SBJ_vars.dirs.proc,SBJ,'_',an_id,'.mat');
 fprintf('===================================================\n');
 fprintf('--- Saving %s ------------------\n',data_out_filename);
 fprintf('===================================================\n');
--- an_vars/HGm_noBsln_sm0_wn100_vars.m
+++ an_vars/HGm_noBsln_sm0_wn100_vars.m
@@ -1,11 +1,11 @@
-event_type  = 'stim';           % event around which to cut trials
+% event_type  = 'stim';           % event around which to cut trials
 % trial_lim_s will NOT be full of data! the first and last t_ftimwin/2 epochs will be NaNs
-trial_lim_s = [-0.25 1.51];      % window in SEC for cutting trials
+% trial_lim_s = [-0.25 1.51];      % window in SEC for cutting trials
 %plt_lim     = [-0.2 1.5];         % window to plot this data
 demean_yn   = 'no';             % z-score for HFA instead
-bsln_evnt   = 'stim';
-bsln_type   = 'zboot';
-bsln_lim    = [-0.25 -0.05];    % window in SEC for baseline correction
+% bsln_evnt   = 'stim';
+% bsln_type   = 'zboot';
+% bsln_lim    = [-0.25 -0.05];    % window in SEC for baseline correction
 
 % HFA Calculations
 HFA_type = 'multiband';                 % b = broadband = 70-150 Hz
@@ -37,7 +37,7 @@ cfg_hfa.keeptrials   = 'yes';                               % must be 'yes' for 
 
 
 % Outlier Rejection
-outlier_std_lim = 6;
+% outlier_std_lim = 6;
 
 % Cleaning up power time series for plotting
 smooth_pow_ts = 0;
@@ -47,25 +47,25 @@ hp_yn       = 'no';
 hp_freq     = 0.5;
 
 % Stats parameters
-stat_lim    = [0 1.5];            % window in SEC for stats
-n_boots     = 1000;             % Repetitions for non-parametric stats
-
-cfg_stat = [];
-cfg_stat.latency          = stat_lim;
-cfg_stat.channel          = 'all';
-cfg_stat.parameter        = 'powspctrm';
-cfg_stat.method           = 'montecarlo';
-cfg_stat.statistic        = 'ft_statfun_indepsamplesT';
-cfg_stat.correctm         = 'cluster';
-cfg_stat.clusteralpha     = 0.05;   %threshold for a single comparison (time point) to be included in the clust
-cfg_stat.clusterstatistic = 'maxsum';
-cfg_stat.clustertail      = 0;
-cfg_stat.tail             = 0; %two sided
-cfg_stat.correcttail      = 'alpha'; %correct the .alpha for two-tailed test (/2)
-cfg_stat.alpha            = 0.05;
-cfg_stat.numrandomization = n_boots;
-cfg_stat.neighbours       = [];%neighbors;
-% cfg_stat.minnbchan        = 0;
-cfg_stat.ivar             = 1;  %row of design matrix containing independent variable
-% cfg_stat.uvar             = 2;  %row containing dependent variable, not needed for indepsamp
+% stat_lim    = [0 1.5];            % window in SEC for stats
+% n_boots     = 1000;             % Repetitions for non-parametric stats
+% 
+% cfg_stat = [];
+% cfg_stat.latency          = stat_lim;
+% cfg_stat.channel          = 'all';
+% cfg_stat.parameter        = 'powspctrm';
+% cfg_stat.method           = 'montecarlo';
+% cfg_stat.statistic        = 'ft_statfun_indepsamplesT';
+% cfg_stat.correctm         = 'cluster';
+% cfg_stat.clusteralpha     = 0.05;   %threshold for a single comparison (time point) to be included in the clust
+% cfg_stat.clusterstatistic = 'maxsum';
+% cfg_stat.clustertail      = 0;
+% cfg_stat.tail             = 0; %two sided
+% cfg_stat.correcttail      = 'alpha'; %correct the .alpha for two-tailed test (/2)
+% cfg_stat.alpha            = 0.05;
+% cfg_stat.numrandomization = n_boots;
+% cfg_stat.neighbours       = [];%neighbors;
+% % cfg_stat.minnbchan        = 0;
+% cfg_stat.ivar             = 1;  %row of design matrix containing independent variable
+% % cfg_stat.uvar             = 2;  %row containing dependent variable, not needed for indepsamp
 
