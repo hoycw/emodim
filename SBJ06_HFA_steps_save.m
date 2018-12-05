a726f5cf21fd68f954cd3eb3699bdc85ad08f97e
hoycw <hoycw@berkeley.edu>
2018-12-05 11:54:18 -0800

    SBJ06_steps to see effects of HFA baselining

---
+++ SBJ06_HFA_steps_save.m
@@ -0,0 +1,123 @@
+function SBJ06_HFA_steps_save(SBJ,pipeline_id,an_id)
+% Calculates high frequency activity and saves fieldtrip struct
+
+%% Set up paths
+if exist('/home/knight/','dir');root_dir='/home/knight/';ft_dir=[root_dir 'hoycw/Apps/fieldtrip/'];
+else root_dir='/Volumes/hoycw_clust/';ft_dir='/Users/colinhoy/Code/Apps/fieldtrip/';end
+
+%% Set Up Directories
+addpath([root_dir 'emodim/scripts/']);
+addpath([root_dir 'emodim/scripts/utils/']);
+addpath(ft_dir);
+ft_defaults
+
+%% Data Preparation
+SBJ_vars_cmd = ['run ' root_dir 'emodim/scripts/SBJ_vars/' SBJ '_vars.m'];
+eval(SBJ_vars_cmd);
+an_vars_cmd = ['run ' root_dir 'emodim/scripts/an_vars/' an_id '_vars.m'];
+eval(an_vars_cmd);
+
+% Load Data
+load(strcat(SBJ_vars.dirs.preproc,SBJ,'_preproc_',pipeline_id,'.mat'));
+load(strcat(SBJ_vars.dirs.events,SBJ,'_trial_info_final.mat'));
+
+%% Select Channel(s)
+cfgs = [];
+cfgs.channel = SBJ_vars.ch_lab.ROI;
+roi = ft_selectdata(cfgs,data);
+
+%% Compute HFA
+fprintf('===================================================\n');
+fprintf('------------------ HFA Calculations ---------------\n');
+fprintf('===================================================\n');
+if strcmp(HFA_type,'multiband')
+    cfg_hfa.trials = 'all';
+    hfa = ft_freqanalysis(cfg_hfa, roi);
+elseif strcmp(HFA_type,'broadband')
+    error('Stop using broadband and use multitapers you dummy!');
+    %         % Filter to HFA band
+    %         cfgpp = [];
+    %         cfgpp.hpfilter  = 'yes';
+    %         cfgpp.hpfreq    = 70;
+    %         cfgpp.lpfilter  = 'yes';
+    %         cfgpp.lpfreq    = 150;
+    %         roi = ft_preprocessing(cfgpp,roi);
+    %         % Hilbert method to extract power
+else
+    error('Unknown HFA_type provided');
+end
+
+data_out_filename = strcat(SBJ_vars.dirs.proc,SBJ,'_',an_id,'_allBands.mat');
+fprintf('===================================================\n');
+fprintf('--- Saving %s ------------------\n',data_out_filename);
+fprintf('===================================================\n');
+save(data_out_filename,'-v7.3','hfa');
+
+%% Baseline Correction
+fprintf('===================================================\n');
+fprintf('---------------- Baseline Correction --------------\n');
+fprintf('===================================================\n');
+switch bsln_type
+    case {'zboot', 'zscore'}
+        hfa = fn_bsln_ft_tfr(hfa,bsln_lim,bsln_type,n_boots);
+    case {'relchange', 'demean', 'my_relchange'}
+        error(['bsln_type ' bsln_type ' is not compatible with one-sample t test bsln activation stats']);
+%         cfgbsln = [];
+%         cfgbsln.baseline     = bsln_lim;
+%         cfgbsln.baselinetype = bsln_type;
+%         cfgbsln.parameter    = 'powspctrm';
+%         hfa = ft_freqbaseline(cfgbsln,hfa);
+    otherwise
+        error(['No baseline implemented for bsln_type: ' bsln_type]);
+end
+
+data_out_filename = strcat(SBJ_vars.dirs.proc,SBJ,'_',an_id,'_bsln.mat');
+fprintf('===================================================\n');
+fprintf('--- Saving %s ------------------\n',data_out_filename);
+fprintf('===================================================\n');
+save(data_out_filename,'-v7.3','hfa');
+
+%% Smooth Power Time Series
+if smooth_pow_ts
+    fprintf('===================================================\n');
+    fprintf('----------------- Filtering Power -----------------\n');
+    fprintf('===================================================\n');
+    for ch_ix = 1:numel(hfa.label)
+        for f_ix = 1:numel(hfa.freq)
+            if strcmp(lp_yn,'yes') && strcmp(hp_yn,'yes')
+                hfa.powspctrm(:,ch_ix,f_ix,:) = fn_EEGlab_bandpass(...
+                    hfa.powspctrm(:,ch_ix,f_ix,:), roi.fsample, hp_freq, lp_freq);
+            elseif strcmp(lp_yn,'yes')
+                hfa.powspctrm(:,ch_ix,f_ix,:) = fn_EEGlab_lowpass(...
+                    squeeze(hfa.powspctrm(:,ch_ix,f_ix,:)), roi.fsample, lp_freq);
+            elseif strcmp(hp_yn,'yes')
+                error('Why are you only high passing?');
+            else
+                error('Why did you say yes smooth but no to both low and high pass?');
+            end
+        end
+    end
+    
+    data_out_filename = strcat(SBJ_vars.dirs.proc,SBJ,'_',an_id,'_sm.mat');
+    fprintf('===================================================\n');
+    fprintf('--- Saving %s ------------------\n',data_out_filename);
+    fprintf('===================================================\n');
+    save(data_out_filename,'-v7.3','hfa');
+end
+
+%% Merge multiple bands
+if strcmp(HFA_type,'multiband')
+    cfg_avg = [];
+    cfg_avg.freq = 'all';
+    cfg_avg.avgoverfreq = 'yes';
+    hfa = ft_selectdata(cfg_avg,hfa);
+end
+
+%% Save Results
+data_out_filename = strcat(SBJ_vars.dirs.proc,SBJ,'_',an_id,'.mat');
+fprintf('===================================================\n');
+fprintf('--- Saving %s ------------------\n',data_out_filename);
+fprintf('===================================================\n');
+save(data_out_filename,'-v7.3','hfa');
+
+end
