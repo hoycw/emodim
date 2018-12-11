0be7b49e07a355d15de5d166f8a826ad519fc714
hoycw <hoycw@berkeley.edu>
2018-12-11 14:22:31 -0800

    fn_get_root_dir bug for app dir on cluster fixed

--- utils/fn_get_root_dir.m
+++ utils/fn_get_root_dir.m
@@ -2,7 +2,7 @@ function [root_dir, app_dir] = fn_get_root_dir()
 %% Check with user/OS/root directory, return relevant locations
 if exist('/home/knight/','dir')
     root_dir = '/home/knight/';
-    app_dir   = [root_dir 'Apps/'];
+    app_dir   = [root_dir 'hoycw/Apps/'];
 elseif exist('/Volumes/hoycw_clust/','dir')
     root_dir = '/Volumes/hoycw_clust/';
     app_dir   = '/Users/colinhoy/Code/Apps/';
