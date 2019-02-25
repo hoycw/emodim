ec06d43c1debc9d6a047467e6753a6f71d88a4c9
hoycw <hoycw@berkeley.edu>
2019-02-25 08:29:04 -0800

    SGE opts for SBJ06 memory increase; run_atlas_assignments update

--- run_lists/run_atlas_assignments.m
+++ run_lists/run_atlas_assignments.m
@@ -1,28 +1,37 @@
-SBJs = {'CP24'};%IR68,
+SBJs = {'CP24','IR61','IR66','IR68','IR71','IR74','IR75','IR78','IR84'};
 
-%% Create basic elec structs
-fn_compile_elec_struct(SBJs{1},'main_ft','pat','');
-fn_compile_elec_struct(SBJs{1},'main_ft','mni','v');
+%% Directories
+if exist('/home/knight/','dir');root_dir='/home/knight/';ft_dir=[root_dir 'hoycw/Apps/fieldtrip/'];
+elseif exist('/Users/lapate/','dir');root_dir = '/Users/lapate/knight/';ft_dir = '/Users/lapate/knight/hoycw/Apps/fieldtrip/';
+else root_dir='/Volumes/hoycw_clust/';ft_dir='/Users/colinhoy/Code/Apps/fieldtrip/';end
+
+%%
+addpath([root_dir 'emodim/scripts/']);
+addpath([root_dir 'emodim/scripts/utils/']);
+addpath(ft_dir);
+ft_defaults
 
 %% Compare to atlases
-for s = 1:numel(SBJs)
+for s = 2:numel(SBJs)
+    fn_compile_elec_struct(SBJs{s},'main_ft','pat','');
+    fn_compile_elec_struct(SBJs{s},'main_ft','mni','v');
     fn_save_elec_atlas(SBJs{s},'main_ft','pat','','DK');
     fn_save_elec_atlas(SBJs{s},'main_ft','pat','','Dx');
-    fn_save_elec_tissue(SBJs{s},'main_ft','pat','','Dx');
-    fn_save_elec_tissue(SBJs{s},'main_ft','pat','','DK');
+%     fn_save_elec_tissue(SBJs{s},'main_ft','pat','','Dx');
+%     fn_save_elec_tissue(SBJs{s},'main_ft','pat','','DK');
 %     fn_save_elec_atlas(SBJs{s},'main_ft','mni','v','Yeo7');
 %     fn_save_elec_atlas(SBJs{s},'main_ft','mni','v','Yeo17');
 end
 
 %% MNI Check
-SBJ = 'IR21';
-
-% Compare patient and MNI in ortho
-fn_view_recon(SBJ,'main_ft','ortho','pat','',1,'b');
-fn_view_recon(SBJ,'main_ft','ortho','mni','v',1,'b');
-
-% Check atlas assignments
-fn_view_recon_atlas(SBJ,pipeline_id,'pat','',1,'b','DK','gROI');
-fn_view_recon_atlas(SBJ,pipeline_id,'pat','',1,'b','Dx','gROI');
-fn_view_recon_atlas(SBJ,pipeline_id,'mni','v',1,'b','Yeo7','Yeo7');
-
+% SBJ = 'IR21';
+% 
+% % Compare patient and MNI in ortho
+% fn_view_recon(SBJ,'main_ft','ortho','pat','',1,'b');
+% fn_view_recon(SBJ,'main_ft','ortho','mni','v',1,'b');
+% 
+% % Check atlas assignments
+% fn_view_recon_atlas(SBJ,pipeline_id,'pat','',1,'b','DK','gROI');
+% fn_view_recon_atlas(SBJ,pipeline_id,'pat','',1,'b','Dx','gROI');
+% fn_view_recon_atlas(SBJ,pipeline_id,'mni','v',1,'b','Yeo7','Yeo7');
+% 
